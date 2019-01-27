b22a03a985b310d01794a35774b7f44e21f3e0ba
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-01-27 14:49:07 +0100

    Added firebase

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <h1>Welcome!</h1>
+        <div>
+            {{loading ? 'Loading...' : people}}
+        </div>
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+<script>
+  import { db } from './main'
+
+  export default {
+    name: 'app',
+    data: () => ({
+      people : null,
+      loading: true
+    }),
+    created() {
+      // this unbinds any previously bound reference
+      this.$bind('people', db.collection('people')).then(() => {
+        // this.todos = todos
+        setTimeout(() => {
+          this.loading = false
+        }, 1000)
+      })
+    }
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2C3E50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
+import firebase from 'firebase'
 import Vue from 'vue'
 import App from './App.vue'
+import VueFire from 'vuefire'
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: "AIzaSyBTIm5ZhdwE5g_mcVsG1VDEG0AwpR4EGe8",
+  authDomain: "lan-plan.firebaseapp.com",
+  databaseURL: "https://lan-plan.firebaseio.com",
+  projectId: "lan-plan",
+  storageBucket: "lan-plan.appspot.com",
+  messagingSenderId: "1030144002720"
+};
+
+firebase.initializeApp(config);
+export const db = firebase.firestore()
+
+Vue.use(VueFire)
 
 new Vue({
   render: h => h(App),
