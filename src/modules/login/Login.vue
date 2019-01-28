c968359555b3b7bd471c31c7af94bf094de098b7
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-01-28 19:21:56 +0100

    Added login page

---
+++ src/modules/login/Login.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+    <div id="firebaseui-auth-container"></div>
+</template>
+
+<script>
+  import firebaseui from 'firebaseui'
+  import firebase   from 'firebase'
+
+  export default {
+    name: 'Login',
+    mounted() {
+      const firebaseUIConfig = {
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ]
+      }
+      const ui = new firebaseui.auth.AuthUI(firebase.auth())
+      ui.start('#firebaseui-auth-container', firebaseUIConfig)
+    },
+  }
+</script>
+
+<style scoped lang="sass">
+    @import "~firebaseui/dist/firebaseui.css"
+</style>
