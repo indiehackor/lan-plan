046665936a88c7ecee69e31e1c65994fe72b0b46
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-01-30 22:13:45 +0100

    Added custom login form

--- src/modules/login/Login.vue
+++ src/modules/login/Login.vue
@@ -1,26 +1,102 @@
 <template lang="html">
-    <div id="firebaseui-auth-container"></div>
+    <div class="login-container">
+        <label
+            for="email"
+            class="input-label"
+        >E-post</label>
+        <input
+            id="email"
+            type="email"
+            class="login-input"
+            v-model="email"
+        >
+        <label
+            for="password"
+            class="input-label"
+        >Passord</label>
+        <input
+            id="password"
+            type="password"
+            class="login-input"
+            v-model="password"
+        >
+        <span>
+            <input
+                id="forgotten-password"
+                type="checkbox"
+                v-model="forgotPassword"
+            >
+            <label for="forgotten-password">
+                Glemt passord
+            </label>
+        </span>
+        <span>
+            <input
+                type="checkbox"
+                id="new-user"
+                v-model="newUser"
+            >
+            <label for="new-user">
+                Ny bruker
+            </label>
+        </span>
+        <button
+            @click="letsGo"
+            id="login-button"
+        >Let's go!
+        </button>
+    </div>
 </template>
 
 <script>
-  import firebaseui from 'firebaseui'
-  import firebase   from 'firebase'
+  import {
+    login,
+    sendResetEmail,
+    registerNewUser
+  } from '../../firebaseApi'
 
   export default {
-    name: 'Login',
-    mounted() {
-      const firebaseUIConfig = {
-        signInOptions: [
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID
-        ]
+    name   : 'Login',
+    data() {
+      return {
+        email         : '',
+        password      : '',
+        forgotPassword: false,
+        newUser       : false
       }
-      const ui = new firebaseui.auth.AuthUI(firebase.auth())
-      ui.start('#firebaseui-auth-container', firebaseUIConfig)
+    },
+    methods: {
+      letsGo () {
+        if (!this.newUser && !this.forgotPassword) {
+          login(this.email, this.password)
+        } else if (this.newUser && !this.forgotPassword) {
+          registerNewUser(this.email, this.password)
+        } else if (!this.newUser && this.forgotPassword && this.email) {
+          sendResetEmail(this.email)
+        } else if (this.newUser && this.forgotPassword) {
+          alert("Nysgjerrigper!")
+        }
+      }
     },
   }
 </script>
 
 <style scoped lang="sass">
-    @import "~firebaseui/dist/firebaseui.css"
+    .login-container
+        display: flex
+        flex-direction: column
+        width: 300px
+        margin: 50px auto
+        align-items: flex-start
+
+        #login-button
+            margin: 10px 0
+            width: 100px
+
+        .login-input
+            width: 100%
+
+        .input-label
+            text-align: left
+
 </style>
