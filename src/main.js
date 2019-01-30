0f3a3c3eb953813b77cf9bb93e014ad07a3aa343
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-01-30 22:14:47 +0100

    Added firebase, router and vuex

--- src/main.js
+++ src/main.js
@@ -1,24 +1,35 @@
-import firebase from 'firebase'
-import Vue from 'vue'
-import App from './App.vue'
-import VueFire from 'vuefire'
+import Vue       from 'vue'
+import VueFire   from 'vuefire'
+import Vuex      from 'vuex'
+import firebase  from 'firebase/app'
+import 'firebase/auth'
+import VueRouter from 'vue-router'
+import router    from './router'
+import App       from './App.vue'
+import store     from './store'
 
 Vue.config.productionTip = false
 
 const config = {
-  apiKey: "AIzaSyBTIm5ZhdwE5g_mcVsG1VDEG0AwpR4EGe8",
-  authDomain: "lan-plan.firebaseapp.com",
-  databaseURL: "https://lan-plan.firebaseio.com",
-  projectId: "lan-plan",
-  storageBucket: "lan-plan.appspot.com",
-  messagingSenderId: "1030144002720"
-};
+  apiKey           : 'AIzaSyBTIm5ZhdwE5g_mcVsG1VDEG0AwpR4EGe8',
+  authDomain       : 'lan-plan.firebaseapp.com',
+  databaseURL      : 'https://lan-plan.firebaseio.com',
+  projectId        : 'lan-plan',
+  storageBucket    : 'lan-plan.appspot.com',
+  messagingSenderId: '1030144002720'
+}
 
-firebase.initializeApp(config);
-export const db = firebase.firestore()
+firebase.initializeApp(config)
+
+export const fb = firebase
+
 
 Vue.use(VueFire)
+Vue.use(VueRouter)
+Vue.use(Vuex)
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
