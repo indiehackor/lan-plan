6cf83a8e876d7273a740fb1b873bb429496ff5a4
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-02-03 14:59:28 +0100

    Added subroutes

--- src/router.js
+++ src/router.js
@@ -1,11 +1,25 @@
 import VueRouter from 'vue-router'
 import Login     from './modules/login/Login'
 import Ratings   from './modules/ratings/Ratings'
+import Rate      from './modules/ratings/components/Rate'
+import Rank      from './modules/Rank/Rank'
 
 const router = new VueRouter({
   mode  : 'history',
   routes: [
-    { path: '/', component: Ratings },
+    {
+      path    : '/', component: Ratings,
+      children: [
+        {
+          path     : 'rate',
+          component: Rate
+        },
+        {
+          path     : 'rank',
+          component: Rank
+        }
+      ]
+    },
     { path: '/login', component: Login }
   ]
 })
