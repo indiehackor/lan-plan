8717e79e99bdd79c6360a0f5543cad49a00d0816
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-02-03 13:29:30 +0100

    Added initial rank item

---
+++ src/modules/Rank/components/RankItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="rank-item">
+        <span class="username">{{user.name}}</span>
+        <star :stars="user.stars"/>
+    </div>
+</template>
+
+<script>
+  import Star from './Star'
+
+  export default {
+    props: ['user'],
+    components: { Star },
+    name      : 'RankItem'
+  }
+</script>
+
+<style scoped lang="sass">
+.rank-item
+    display: flex
+    justify-content: space-between
+    margin-bottom: 10px
+.username
+    padding: 5px
+</style>
