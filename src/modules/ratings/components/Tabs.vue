c8f939711a2fba8a20e5fe318663a18b067eca9e
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-02-03 14:59:03 +0100

    Added tabs components

---
+++ src/modules/ratings/components/Tabs.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="tab-container">
+        <button @click="rate">
+            RATE
+        </button>
+        <button @click="rank">
+            RANKING
+        </button>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Tabs',
+    methods: {
+      rate() {
+        this.$router.push('/rate')
+      },
+      rank() {
+        this.$router.push('/rank')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
