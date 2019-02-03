3ed71e07ed6837696a5e6a04d3d0ea346d9f0ab7
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-02-03 15:23:00 +0100

    Added Rate view

---
+++ src/modules/Rate/Rate.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="list-container">
+        <rate-item v-for="user in users" :key="user.displayName" :user="user"/>
+    </div>
+</template>
+
+<script>
+  import RateItem from './components/RateItem'
+
+  export default {
+    name      : 'Rate',
+    components: { RateItem },
+    data() {
+      return {
+        test : 'lol',
+        users: [
+          { displayName: 'CrzyDck', stars: 3 },
+          { displayName: '9do', stars: 1 },
+          { displayName: 'cmdkeen', stars: 5 },
+          { displayName: 'Lubricator', stars: 4 }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="sass">
+    .list-container
+        width: 500px
+        margin: 10px auto
+</style>
