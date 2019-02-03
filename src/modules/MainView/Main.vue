6256cc1d80526adbf11207fb95e80cb0fbbca953
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-02-03 15:25:49 +0100

    Main view

---
+++ src/modules/MainView/Main.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="ratings">
+        <h1>Welcome to LanPlan {{ displayName }}!</h1>
+        <tabs/>
+        <router-view/>
+    </div>
+</template>
+
+
+<script>
+  import { mapState } from 'vuex'
+  import Tabs         from './components/Tabs'
+
+  export default {
+    name      : 'Ratings',
+    components: { Tabs },
+    computed  : mapState({
+      displayName: state => state.user ? state.user.displayName : null
+    })
+  }
+
+</script>
+
+<style>
+
+</style>
