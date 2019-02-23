4c05d61bf1c22dd9675229e35717832b600ae0c4
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-02-23 21:35:50 +0100

    Show remaining ratings in navbar

--- src/modules/NavBar/NavBar.vue
+++ src/modules/NavBar/NavBar.vue
@@ -1,8 +1,14 @@
 <template>
     <div id="nav-bar">
-        <span class="email">
+        <section class="email">
             {{email}}
-        </span>
+            <section class="info">
+                <h2>har igjen</h2>
+                <span><star/> {{stars}}</span>
+                <span><honour/> {{honours}}</span>
+                <span><thumb/> {{thumbsDown}}</span>
+            </section>
+        </section>
         <confirm-users-list v-if="admin"/>
         <div>
             <give-ratings v-if="admin"/>
@@ -17,10 +23,13 @@
   import { mapState, mapActions } from 'vuex'
   import ConfirmUsersList         from './components/ConfirmUsersList'
   import GiveRatings from './components/GiveRatings'
+  import Honour from '../../components/icons/Honour'
+  import Star from '../../components/icons/Star'
+  import Thumb from '../../components/icons/ThumbDown'
 
   export default {
     name      : 'NavBar',
-    components: { ConfirmUsersList, GiveRatings },
+    components: { ConfirmUsersList, GiveRatings, Star, Honour, Thumb},
     methods   : {
       ...mapActions([
         'signOutCurrentUser'
@@ -28,12 +37,36 @@
     },
     computed  : mapState({
       email: state => state.user ? state.user.email : '',
-      admin: state => state.user ? state.user.admin : false
+      admin: state => state.user ? state.user.admin : false,
+      stars: state => state.user ? state.user.stars : 0,
+      honours: state => state.user ? state.user.honours : 0,
+      thumbsDown: state => state.user ? state.user.thumbsDown : 0,
     })
   }
 </script>
 
 <style lang="sass" scoped>
+    svg
+        margin-right: 10px
+    .email
+        position: relative
+        .info
+            position: absolute
+            width: 100%
+            left: 0
+            top: 100%
+            display: flex
+            flex-direction: column
+            align-items: flex-start
+            height: 0
+            overflow: hidden
+            transition: 500ms
+            span
+                height: 30px
+                display: flex
+        &:hover
+            .info
+                height: 300px
     #nav-bar
         height: 70px
         position: absolute
