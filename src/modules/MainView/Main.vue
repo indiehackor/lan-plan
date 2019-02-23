b1b1e728623668a6ff018f83bb15bb94bd415a55
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-02-23 20:35:14 +0100

    Remove test icons from Main

--- src/modules/MainView/Main.vue
+++ src/modules/MainView/Main.vue
@@ -10,9 +10,6 @@
 <script>
   import { mapState } from 'vuex'
   import Tabs         from './components/Tabs'
-  import Star         from '../../components/icons/Star'
-  import Honour         from '../../components/icons/Honour'
-  import ThumbDown    from '../../components/icons/ThumbDown'
 
   export default {
     name      : 'Ratings',
