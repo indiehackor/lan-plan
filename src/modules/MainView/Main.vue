2a4a7fe3ecfcbb227c7ca2fc5b2f2a0130ae51a1
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-02-23 20:33:38 +0100

    Add icons

--- src/modules/MainView/Main.vue
+++ src/modules/MainView/Main.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="ratings">
         <h1>Velkommen til LanPlan{{ ` ${user.username}` }}!</h1>
+        <star/>
+        <thumb-down/>
+        <honour/>
         <tabs/>
         <router-view/>
     </div>
@@ -8,15 +11,18 @@
 
 
 <script>
-  import { mapState }             from 'vuex'
-  import Tabs                     from './components/Tabs'
+  import { mapState } from 'vuex'
+  import Tabs         from './components/Tabs'
+  import Star         from '../../components/icons/Star'
+  import Honour         from '../../components/icons/Honour'
+  import ThumbDown    from '../../components/icons/ThumbDown'
 
   export default {
     name      : 'Ratings',
-    components: { Tabs },
+    components: { ThumbDown, Star, Tabs, Honour },
     computed  : {
       ...mapState(['user', 'users'])
-    },
+    }
   }
 
 </script>
