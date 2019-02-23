a422fcb1886ff5a8cae28423f2afde2793ea2cb1
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-02-23 17:37:50 +0100

    Formatting

--- src/modules/Rank/Rank.vue
+++ src/modules/Rank/Rank.vue
@@ -13,47 +13,49 @@
 </template>
 
 <script>
-import RankItem from "./components/RankItem";
-import { mapState } from "vuex";
+  import RankItem     from './components/RankItem'
+  import { mapState } from 'vuex'
 
-export default {
-  components: { RankItem },
-  name: "Rank",
-  data() {
-    return {
-      sortCriteria: 'stars'
-    }
-  },
-  methods: {
-    sortByStar() {
-      this.sortCriteria = 'stars'
+  export default {
+    components: { RankItem },
+    name      : 'Rank',
+    data() {
+      return {
+        sortCriteria: 'stars'
+      }
     },
-    sortByHonour() {
-      this.sortCriteria = 'honours'
+    methods   : {
+      sortByStar() {
+        this.sortCriteria = 'stars'
+      },
+      sortByHonour() {
+        this.sortCriteria = 'honours'
+      },
+      sortByThumbsDown() {
+        this.sortCriteria = 'thumbsDown'
+      }
     },
-    sortByThumbsDown() {
-      this.sortCriteria = 'thumbsDown'
-    }
-  },
-  computed: {
-    ...mapState(["users"]),
-    sortedUsers() {
-      return this.users.slice().sort((user1, user2) => {
-        return user2[this.sortCriteria] - user1[this.sortCriteria];
-      });
+    computed  : {
+      ...mapState(['users']),
+      sortedUsers() {
+        return this.users.slice().sort((user1, user2) => {
+          return user2[this.sortCriteria] - user1[this.sortCriteria]
+        })
+      }
     }
   }
-};
 </script>
 
 <style scoped lang="sass">
-.list-container
-    width: 600px
-    margin: 10px auto
-.sort
-    display: flex
-    justify-content: center
-    > *
-        margin: 5px
+    .list-container
+        width: 600px
+        margin: 10px auto
+
+    .sort
+        display: flex
+        justify-content: center
+
+        > *
+            margin: 5px
 
 </style>
