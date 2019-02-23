bfa45073ff087f34eb3ecc4be8cd98f873f2d1d8
vegarf <vegard.hellandflatoy@drift.oslo.kommune.no>
2019-02-23 17:55:49 +0100

    Move populate function to main.js

--- src/App.vue
+++ src/App.vue
@@ -9,84 +9,80 @@
 
 
 <script>
-  import NavBar                   from './modules/NavBar/NavBar'
-  import { fb }                   from './main'
-  import { mapActions, mapState } from 'vuex'
-  import Modal                    from './modules/Modal/Modal'
-  import { populateUsersArray }   from './firebaseApi'
+import NavBar from "./modules/NavBar/NavBar";
+import { fb } from "./main";
+import { mapActions, mapState } from "vuex";
+import Modal from "./modules/Modal/Modal";
 
-  export default {
-    name      : 'app',
-    components: { NavBar, Modal },
-    beforeCreate() {
-      fb.auth().onAuthStateChanged(user => {
-        this.checkUser(user)
-      })
-    },
-    methods   : {
-      ...mapActions(['setCurrentUser', 'stopLoading']),
-      checkUser(user) {
-        if (user) {
-          this.setCurrentUser(user)
-        } else {
-          this.$router.push('/login')
-        }
-        this.stopLoading()
+export default {
+  name: "app",
+  components: { NavBar, Modal },
+  beforeCreate() {
+    fb.auth().onAuthStateChanged(user => {
+      this.checkUser(user);
+    });
+  },
+  methods: {
+    ...mapActions(["setCurrentUser", "stopLoading"]),
+    checkUser(user) {
+      if (user) {
+        this.setCurrentUser(user);
+      } else {
+        this.$router.push("/login");
       }
-    },
-    computed  : {
-      ...mapState(['loading'])
-    },
-    created() {
-      populateUsersArray()
+      this.stopLoading();
     }
+  },
+  computed: {
+    ...mapState(["loading"])
   }
+};
 </script>
 
 <style>
-    #app {
-        font-family: "Muli", sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        margin-top: 70px;
-        padding: 40px;
-    }
+#app {
+  font-family: "Muli", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 70px;
+  padding: 40px;
+}
 
-    h1,
-    h2 {
-        text-transform: uppercase;
-        line-height: 0.8;
-    }
+h1,
+h2 {
+  text-transform: uppercase;
+  line-height: 0.8;
+}
 
-    h1 {
-        font-size: 40px;
-        font-weight: 200;
-        letter-spacing: 4px;
-    }
+h1 {
+  font-size: 40px;
+  font-weight: 200;
+  letter-spacing: 4px;
+}
 
-    h2 {
-        font-size: 20px;
-        font-weight: 300;
-        letter-spacing: 2px;
-    }
+h2 {
+  font-size: 20px;
+  font-weight: 300;
+  letter-spacing: 2px;
+}
 
-    body {
-        background: linear-gradient(270deg, #EA49AB, #2E8EDD);
-        background-size: 400% 400%;
-        animation: AnimationName 60s ease infinite;
-        color: white;
-    }
+body {
+  background: linear-gradient(270deg, #ea49ab, #2e8edd);
+  background-size: 400% 400%;
+  animation: AnimationName 60s ease infinite;
+  color: white;
+}
 
-    @keyframes AnimationName {
-        0% {
-            background-position: 0 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0 50%;
-        }
-    }
+@keyframes AnimationName {
+  0% {
+    background-position: 0 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0 50%;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,37 +1,38 @@
-import Vue       from 'vue'
-import VueFire   from 'vuefire'
-import Vuex      from 'vuex'
-import firebase  from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-import VueRouter from 'vue-router'
-import router    from './router'
-import App       from './App.vue'
-import store     from './store'
+import Vue from "vue";
+import VueFire from "vuefire";
+import Vuex from "vuex";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import VueRouter from "vue-router";
+import router from "./router";
+import App from "./App.vue";
+import store from "./store";
+import { populateUsersArray } from "./firebaseApi";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 const config = {
-  apiKey           : 'AIzaSyBTIm5ZhdwE5g_mcVsG1VDEG0AwpR4EGe8',
-  authDomain       : 'lan-plan.firebaseapp.com',
-  databaseURL      : 'https://lan-plan.firebaseio.com',
-  projectId        : 'lan-plan',
-  storageBucket    : 'lan-plan.appspot.com',
-  messagingSenderId: '1030144002720'
-}
+  apiKey: "AIzaSyBTIm5ZhdwE5g_mcVsG1VDEG0AwpR4EGe8",
+  authDomain: "lan-plan.firebaseapp.com",
+  databaseURL: "https://lan-plan.firebaseio.com",
+  projectId: "lan-plan",
+  storageBucket: "lan-plan.appspot.com",
+  messagingSenderId: "1030144002720"
+};
 
-firebase.initializeApp(config)
+firebase.initializeApp(config);
 
-export const db = firebase.firestore()
-export const fb = firebase
+export const db = firebase.firestore();
+export const fb = firebase;
 
-
-Vue.use(VueFire)
-Vue.use(VueRouter)
-Vue.use(Vuex)
+Vue.use(VueFire);
+Vue.use(VueRouter);
+Vue.use(Vuex);
+populateUsersArray();
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
