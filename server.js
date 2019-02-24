181c30d8db05c40cff8714400ca4ec2045cf8346
vhflat <vhflat@gmail.com>
2019-02-24 20:43:17 +0100

    Force TLS

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-const express = require('express');
-const path = require('path');
-const history = require('connect-history-api-fallback');
+const express = require('express')
+const path    = require('path')
+const history = require('connect-history-api-fallback')
+const enforce = require('express-sslify')
 
-const app = express();
+const app = express()
+// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
+// a load balancer (e.g. Heroku). See further comments below
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
 
-app.use(staticFileMiddleware);
+app.use(staticFileMiddleware)
 app.use(history({
   disableDotRule: false,
-  verbose: true,
-  rewrites: [
-    { from: '/.well-known/acme-challenge/zS6LSAsjsg4o5mw3seW-Qv92tPY2lfkTGXZRU0mlQLs', to: '/cert.html'}
+  verbose       : true,
+  rewrites      : [
+    { from: '/.well-known/acme-challenge/zS6LSAsjsg4o5mw3seW-Qv92tPY2lfkTGXZRU0mlQLs', to: '/cert.html' }
   ]
-}));
-app.use(staticFileMiddleware);
+}))
+app.use(staticFileMiddleware)
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist'));
-});
+  res.render(path.join(__dirname + '/dist'))
+})
 
 var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+  var port = server.address().port
+  console.log('App now running on port', port)
+})
